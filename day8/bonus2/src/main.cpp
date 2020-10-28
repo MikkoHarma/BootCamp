c4b580e86bee333018e66cb1b164d5b041bc818e
Mikko Härmä <mikko.harma@volvocars.com>
2020-10-28 16:58:20 +0100

    Added finished bonus 2

--- day8/bonus2/src/main.cpp
+++ day8/bonus2/src/main.cpp
@@ -1,5 +1,6 @@
 #include "include.h"
 #include "pcapOpenner.h"
+#include <iomanip>
 
 int main(){
 
@@ -11,6 +12,22 @@ int main(){
     packet = pcap.getPacket();
 
     std::cout << "Packet size: " << packet.data_len << std::endl;
+    std::cout << "MAC destination address: ";    
+    for(int i = 0; i < 6; i++){
+         std::cout << std::setfill('0') << std::setw(2) << std::hex <<(int)(packet.data[i]);   
+         i < 5 ? std::cout << ":" : std::cout << "";
+    }
+    std::cout << std::endl;  
+
+    std::cout << "MAC source address: ";    
+    for(int i = 6; i < 12; i++){
+         std::cout << std::setfill('0') << std::setw(2) << std::hex <<(int)(packet.data[i]);   
+         i < 11 ? std::cout << ":" : std::cout << "";
+    }
+    std::cout << std::endl;        
+    
+    std::cout << "Ether Type: " << std::setfill('0') << std::setw(2) << std::hex << (int)(packet.data[12]) << " " 
+        << std::setfill('0') << std::setw(2) << std::hex << (int)(packet.data[13]) << std::endl; 
 
     return 0;
 }
